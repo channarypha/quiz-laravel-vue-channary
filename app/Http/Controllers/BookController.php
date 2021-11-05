fdaedb604da7a591da2a0dcb31c68e61bd1ce314
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-11-05 11:38:20 +0700

    book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,7 +13,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        return Book::last()->get();
+        return Book::with('author')->last()->get();
     }
 
     /**
